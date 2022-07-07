cea3d909321afa25dca1c2198a6f5aadf0989b2d
haireal007 <you@example.com>
2022-07-07 13:54:35 +0700

    hai2

--- src/main/java/controller/filter/CheckLogin.java
+++ src/main/java/controller/filter/CheckLogin.java
@@ -1,15 +1,15 @@
 package controller.filter;
 
+import dao.AccountDao;
+import model.Account;
 import model.Login;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.time.LocalDate;
-
-@WebFilter(urlPatterns = "/student")
+@WebFilter (urlPatterns = {"/student"})
 public class CheckLogin implements Filter {
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -17,11 +17,16 @@ public class CheckLogin implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (Login.name == null){
+        if (Login.account == null) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
-            dispatcher.forward(request,response);
-        }else {
-            chain.doFilter(request,response);
+            dispatcher.forward(request, response);
+        } else {
+            if (Login.account.getRole().equals("user")) {
+                chain.doFilter(request, response);
+            } else {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/error.jsp");
+                dispatcher.forward(request, response);
+            }
         }
     }
 
@@ -30,4 +35,3 @@ public class CheckLogin implements Filter {
         Filter.super.destroy();
     }
 }
-
